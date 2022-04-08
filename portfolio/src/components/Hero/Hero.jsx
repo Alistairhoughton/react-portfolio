42054718ce02a7aea87c1d886647f9d6fc71c301
Alistair Houghton <alistair.g.houghton@gmail.com>
2022-04-08 21:46:58 +0100

    added in code

---
+++ portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import HeroStyles from "./Hero.css";
+
+export default function Hero() {
+  return (
+    <section className="hero-container">
+      <div className="title-holder">
+        <p></p>
+        <p></p>
+      </div>
+    </section>
+  );
+}
---
+++ portfolio/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navstyles from "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="nav-bar">
+      <div className="nav-container">
+        <a href="">Projects</a>
+        <a href="">About</a>
+        <a href="">Contact</a>
+      </div>
+    </nav>
+  );
+}
